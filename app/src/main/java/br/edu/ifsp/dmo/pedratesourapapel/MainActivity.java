8fd5f723721c541ee5ac1b7ff1cd4c203a865cc5
Lucas <lucasalexandre00@hotmail.com>
2023-10-24 10:11:00 -0300

    Commit Main activity

--- app/src/main/java/br/edu/ifsp/dmo/pedratesourapapel/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/dmo/pedratesourapapel/MainActivity.java
@@ -1,14 +1,73 @@
 package br.edu.ifsp.dmo.pedratesourapapel;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.SyncStateContract;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner mSpinner;
+    private Button mButton;
+    private EditText nome1EditText;
+    private EditText nome2EditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar toolbar = getSupportActionBar();
+        if (toolbar != null){
+            toolbar.hide();
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_dropdown_item,
+                getResources().getStringArray(R.array.tipos_jogos)
+        );
+
+        mSpinner = findViewById(R.id.spinner_jogadas);
+        mSpinner.setAdapter(adapter);
+
+        nome1EditText = findViewById(R.id.edittext_jogador1);
+        nome2EditText = findViewById(R.id.edittext_jogador2);
+
+        mButton = findViewById(R.id.button_iniciar);
+        mButton.setOnClickListener( view -> iniciarJogo());
+    }
+
+    private void iniciarJogo() {
+        String player1, player2;
+        int batalhas = 1;
+
+        player1 = nome1EditText.getText().toString();
+        player2 = nome2EditText.getText().toString();
+
+        switch (mSpinner.getSelectedItemPosition()){
+            case 0:
+                batalhas = 1;
+                break;
+            case 1:
+                batalhas = 3;
+                break;
+            case 2:
+                batalhas = 5;
+                break;
+        }
+        Intent intent = new Intent(this, ConfrontoActivity.class);
+        Bundle args = new Bundle();
+        args.putString(Constantes.KEY_JOGADOR_1, player1);
+        args.putString(Constantes.KEY_JOGADOR_2, player2);
+        args.putString(Constantes.KEY_RODADAS, batalhas);
+        intent.putExtra(args);
+        startActivity(intent);
     }
 }
